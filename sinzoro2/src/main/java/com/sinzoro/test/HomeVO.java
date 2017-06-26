0a1810d5175061f79c2cd49c0b4cf8493f98c302
dezcao <dezcao@naver.com>
2017-06-26 16:25:42 +0900

    lombok 추가함.

--- sinzoro2/src/main/java/com/sinzoro/test/HomeVO.java
+++ sinzoro2/src/main/java/com/sinzoro/test/HomeVO.java
@@ -1,20 +1,12 @@
 package com.sinzoro.test;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class HomeVO {
 	
 	private int id;
 	private String name;
 	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
 }
